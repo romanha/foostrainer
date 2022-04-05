a93e59e605060f60a4bb5fdbe7eb4d1cb00b82d4
Roman Habitzl <Roman.Habitzl@gmx.net>
2022-04-05 19:15:31 +0200

    extend MIT license with Roman Habitzl as copyright holder make settings and routine collapsible improve style

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Foostrainer is a simple web-based foosball training application.
 
 Currently, it supports a basic randomized routine of passes and shots.
 
-This app is based on [bitkid's foostrainer](https://github.com/bitkid/foostrainer).
+This application is based on [bitkid's Foostrainer](https://github.com/bitkid/foostrainer).
--- resources/js/script.js
+++ resources/js/script.js
@@ -1,9 +1,16 @@
-let startButton = document.querySelector('#start-training');
-let stopButton = document.querySelector('#stop-training');
+// Routine reference
+let routine = null;
+
+// Control elements
+let startButton = document.getElementById("start-training-button");
+let stopButton = document.getElementById("stop-training-button");
+
+// Settings
+let settingsButton = document.getElementById("settings-button")
+let routineButton = document.getElementById("routine-button")
 let wakeLock = document.querySelector('#wake-lock');
 let goalDelay = document.querySelector('#goal-delay');
 let goalDelayLabel = document.querySelector('#goal-delay-label');
-let routine = null;
 
 function setup() {
     stopButton.disabled = true;
@@ -15,6 +22,28 @@ function setup() {
 }
 
 window.onload = setup;
+
+settingsButton.onclick = function () {
+    let settingsMenu = document.getElementById("settings");
+    if (settingsMenu.style.display === "block") {
+        settingsButton.innerHTML = "&blacktriangleright; Settings";
+        settingsMenu.style.display = "none";
+    } else {
+        settingsButton.innerHTML = "&blacktriangledown; Settings";
+        settingsMenu.style.display = "block";
+    }
+}
+
+routineButton.onclick = function () {
+    let routineMenu = document.getElementById("routine");
+    if (routineMenu.style.display === "block") {
+        routineButton.innerHTML = "&blacktriangleright; Routine";
+        routineMenu.style.display = "none";
+    } else {
+        routineButton.innerHTML = "&blacktriangledown; Routine";
+        routineMenu.style.display = "block";
+    }
+}
 
 goalDelay.onchange = function () {
     goalDelayLabel.textContent = "Goal reset delay: " + goalDelay.value + " seconds";
